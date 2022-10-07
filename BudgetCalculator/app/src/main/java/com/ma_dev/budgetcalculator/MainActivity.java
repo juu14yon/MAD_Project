017496722521c7f7ae483a30cbc8ff7e919243ef
juu14yon <75063827+juu14yon@users.noreply.github.com>
2022-10-07 13:06:33 +0600

    NavBar v2
    Navbar binding to fragments

--- BudgetCalculator/app/src/main/java/com/ma_dev/budgetcalculator/MainActivity.java
+++ BudgetCalculator/app/src/main/java/com/ma_dev/budgetcalculator/MainActivity.java
@@ -1,14 +1,49 @@
 package com.ma_dev.budgetcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.ma_dev.budgetcalculator.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFrame(new HomeFragment());
+
+        binding.navBar.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.homeMenu:
+                    replaceFrame(new HomeFragment());
+                    break;
+                case R.id.historyMenu:
+                    replaceFrame(new HistoryFragment());
+                    break;
+                case R.id.statsMenu:
+                    replaceFrame(new StatsFragment());
+                    break;
+                case R.id.settingsMenu:
+                    replaceFrame(new SettingsFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFrame(Fragment frag){
+        FragmentManager fragManager = getSupportFragmentManager();
+        FragmentTransaction fragTransaction = fragManager.beginTransaction();
+        fragTransaction.replace(R.id.frameLayout, frag);
+        fragTransaction.commit();
     }
 }
